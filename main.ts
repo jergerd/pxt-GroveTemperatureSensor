0a594e9bb234285823072391179735c1e5345999
jergerd <109340374+jergerd@users.noreply.github.com>
2023-02-04 13:22:22 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,26 @@
-//% color="#14de46" weight=100
-namespace gorve_temperature {
+//% color="#002050" weight=100
+namespace TemperaturSensor {
+
+    let ntc = 0
+    let temperatureKelvin = 0
+
+    function getTemperature_GroveSensor (anaVal: number) {
+        anaVal = anaVal / 1023 * 3300
+        ntc = (3300 / anaVal - 1) * 10000
+        temperatureKelvin = 1 / (1 / 298.15 + 1 / 4275 * Math.log(ntc/10000))
+        return temperatureKelvin
+    }
 
     //% block
-    export function TemperaturCelsius(): number {
-      function getTemperature_GroveSensor (anaVal: number) {
-        anaVal = anaVal / 1023 * 3300
-        ntc = (3300 / valbet - 1) * 10000
-        temperatureKelvin = 1 / (1 / 298.15 + 1 / 4275 * Math.log(ntc/10000))
-        temperatureCelcius = temperatureKelvin - 273.15
+    export function TemperaturCelsius(): number {  
+        let analogValueA1 = pins.analogReadPin(AnalogPin.C16)
+        let temperatureCelcius = getTemperature_GroveSensor(analogValueA1)-273.15
         return temperatureCelcius
-      }
+    }
 
-      let analogValueA1 = 0
-      let ntc = 0
-      let temperatureKelvin = 0
-      let temperatureCelcius = 0
-
-      analogValueA1 = pins.analogReadPin(AnalogPin.C16)
-      return getTemperature_GroveSensor(analogValueA1)
+    //% block
+    export function TemperaturKelvin(): number {  
+        let analogValueA1 = pins.analogReadPin(AnalogPin.C16)
+        return getTemperature_GroveSensor(analogValueA1)
     }
 }
